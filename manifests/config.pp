9d6753ff71916476307d55affa92ac975aa2f227
call <6537446+call@users.noreply.github.com>
2020-02-05 16:44:42 -0800

    Add config boilerplate

---
+++ manifests/config.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include buildkite_agent::config
+class buildkite_agent::config {
+}
