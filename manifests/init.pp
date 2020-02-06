b9a8a585d171ae4bef93f26e7966cc233ba91040
call <6537446+call@users.noreply.github.com>
2020-02-05 20:09:46 -0800

    Test create_resources

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,11 +4,22 @@
 #
 # @example
 #   include buildkite_agent
-class buildkite_agent {
+class buildkite_agent (
+  Optional[Hash[String, Hash[String, Variant[String, Integer, Boolean]]]] $configs,
+) {
   include archive
   include buildkite_agent::install
 
-  buildkite_agent::config { 'primary':
-    tags => 'queue=mini',
-  }
+  create_resources(buildkite_agent::config, $configs)
+
+  # if $configs {
+  #   $configs.each |String $key, Hash[String, Variant[String, Integer, Boolean]]]
+
+  #   buildkite_agent::config { $key:
+  #   tags => 'queue=mini',
+  # }
+  # }
+  # buildkite_agent::config { 'primary':
+  #   tags => 'queue=mini',
+  # }
 }
