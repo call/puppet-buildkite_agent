4718a36488dc44dde1048761c9dd0485d80f2201
call <6537446+call@users.noreply.github.com>
2020-02-05 19:20:30 -0800

    Use file_line

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,13 +1,11 @@
 # @summary Manages buildkite-agent configuration.
 #
-# Manages configuration for buildkite-agent
+# Manages a Buildkite agent config file
 #
 # @example
 #   buildkite_agent::config { 'namevar': }
 define buildkite_agent::config (
-  String[1] $path = '/usr/local/etc/buildkite-agent/buildkite-agent.cfg',
-  String[1] $hooks = '/usr/local/etc/buildkite-agent/hooks',
-  String[1] $builds = '/usr/local/var/buildkite-agent/builds',
+  String[1] $config_file_path = '/usr/local/etc/buildkite-agent/buildkite-agent.cfg',
   Optional[String] $bootstrap_script = undef,
   Optional[String] $build_path = undef,
   Optional[String] $cancel_grace_period = undef,
@@ -54,65 +52,70 @@ define buildkite_agent::config (
   Optional[String] $wait_for_gcp_labels_timeout = undef,
 ) {
 
-  file { '/usr/local/etc/buildkite-agent/':
+  file { dirname($config_file_path):
     ensure => directory,
   }
 
-  $defaults = {
-    'path'   => $path,
-    'section' => undef,
+  file { $config_file_path:
+    ensure => file,
   }
 
   $settings = {
-    'main' => {
-      'bootstrap-script'              => $bootstrap_script,
-      'build-path'                    => $build_path,
-      'cancel-grace-period'           => $cancel_grace_period,
-      'cancel-signal'                 => $cancel_signal,
-      'debug'                         => $debug,
-      'debug-http'                    => $debug_http,
-      'disconnect-after-job'          => $disconnect_after_job,
-      'disconnect-after-idle-timeout' => $disconnect_after_idle_timeout,
-      'endpoint'                      => $endpoint,
-      'experiment'                    => $experiment,
-      'git-clean-flags'               => $git_clean_flags,
-      'git-clone-flags'               => $git_clone_flags,
-      'git-clone-mirror-flags'        => $git_clone_mirror_flags,
-      'git-fetch-flags'               => $git_fetch_flags,
-      'git-mirrors-lock-timeout'      => $git_mirrors_lock_timeout,
-      'git-mirrors-path'              => $git_mirrors_path,
-      'health-check-addr'             => $health_check_addr,
-      'hooks-path'                    => $hooks_path,
-      'log-format'                    => $log_format,
-      'metrics-datadog'               => $metrics_datadog,
-      'metrics-datadog-host'          => $metrics_datadog_host,
-      'name'                          => $bk_name,
-      'no-color'                      => $no_color,
-      'no-command-eval'               => $no_command_eval,
-      'no-git-submodules'             => $no_git_submodules,
-      'no-http2'                      => $no_http2,
-      'no-local-hooks'                => $no_local_hooks,
-      'no-plugins'                    => $no_plugins,
-      'no-plugin-validation'          => $no_plugin_validation,
-      'no-pty'                        => $no_pty,
-      'no-ssh-keyscan'                => $no_ssh_keyscan,
-      'plugins-path'                  => $plugins_path,
-      'priority'                      => $priority,
-      'shell'                         => $shell,
-      'spawn'                         => $spawn,
-      'tags'                          => $tags,
-      'tags-from-ec2'                 => $tags_from_ec2,
-      'tags-from-ec2-tags'            => $tags_from_ec2_tags,
-      'tags-from-gcp'                 => $tags_from_gcp,
-      'tags-from-gcp-labels'          => $tags_from_gcp_labels,
-      'tags-from-host'                => $tags_from_host,
-      'timestamp-lines'               => $timestamp_lines,
-      'wait-for-ec2-tags-timeout'     => $wait_for_ec2_tags_timeout,
-      'wait-for-gcp-labels-timeout'   => $wait_for_gcp_labels_timeout,
-    }
+    'bootstrap-script'              => $bootstrap_script,
+    'build-path'                    => $build_path,
+    'cancel-grace-period'           => $cancel_grace_period,
+    'cancel-signal'                 => $cancel_signal,
+    'debug'                         => $debug,
+    'debug-http'                    => $debug_http,
+    'disconnect-after-job'          => $disconnect_after_job,
+    'disconnect-after-idle-timeout' => $disconnect_after_idle_timeout,
+    'endpoint'                      => $endpoint,
+    'experiment'                    => $experiment,
+    'git-clean-flags'               => $git_clean_flags,
+    'git-clone-flags'               => $git_clone_flags,
+    'git-clone-mirror-flags'        => $git_clone_mirror_flags,
+    'git-fetch-flags'               => $git_fetch_flags,
+    'git-mirrors-lock-timeout'      => $git_mirrors_lock_timeout,
+    'git-mirrors-path'              => $git_mirrors_path,
+    'health-check-addr'             => $health_check_addr,
+    'hooks-path'                    => $hooks_path,
+    'log-format'                    => $log_format,
+    'metrics-datadog'               => $metrics_datadog,
+    'metrics-datadog-host'          => $metrics_datadog_host,
+    'name'                          => $bk_name,
+    'no-color'                      => $no_color,
+    'no-command-eval'               => $no_command_eval,
+    'no-git-submodules'             => $no_git_submodules,
+    'no-http2'                      => $no_http2,
+    'no-local-hooks'                => $no_local_hooks,
+    'no-plugins'                    => $no_plugins,
+    'no-plugin-validation'          => $no_plugin_validation,
+    'no-pty'                        => $no_pty,
+    'no-ssh-keyscan'                => $no_ssh_keyscan,
+    'plugins-path'                  => $plugins_path,
+    'priority'                      => $priority,
+    'shell'                         => $shell,
+    'spawn'                         => $spawn,
+    'tags'                          => $tags,
+    'tags-from-ec2'                 => $tags_from_ec2,
+    'tags-from-ec2-tags'            => $tags_from_ec2_tags,
+    'tags-from-gcp'                 => $tags_from_gcp,
+    'tags-from-gcp-labels'          => $tags_from_gcp_labels,
+    'tags-from-host'                => $tags_from_host,
+    'timestamp-lines'               => $timestamp_lines,
+    'wait-for-ec2-tags-timeout'     => $wait_for_ec2_tags_timeout,
+    'wait-for-gcp-labels-timeout'   => $wait_for_gcp_labels_timeout,
   }
 
-  create_ini_settings($settings, $defaults)
+  $settings.each |String $key, Optional[String] $val| {
+    if $val {
+      file_line { "setting_${key}":
+        path => $config_file_path,
+        line => "${key}=${val}",
+      }
+    }
+
+  }
 
 }
 
