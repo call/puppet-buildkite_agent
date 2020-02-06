ceb0cc8e6d891f571609c3d82de767f1b400421e
call <6537446+call@users.noreply.github.com>
2020-02-05 21:35:40 -0800

    Add defined type boilerplate

---
+++ spec/defines/service_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'buildkite_agent::service' do
+  let(:title) { 'namevar' }
+  let(:params) do
+    {}
+  end
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+    end
+  end
+end
