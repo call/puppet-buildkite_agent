0bd597c0a5b65841b7d59649f079a9a9e844775c
call <6537446+call@users.noreply.github.com>
2020-02-10 00:37:50 -0800

    Add more tests

--- test/spec/localhost/mac_spec.rb
+++ test/spec/localhost/mac_spec.rb
@@ -2,19 +2,64 @@ require 'spec_helper'
 
 set :backend, :exec
 
-describe service('com.buildkite.buildkite-agent-primary'), :if => os[:family] == 'darwin' do
-  # it { should be_running } # TODO: This is incorrectly using ps aux to look for service
-  # it { should be_running.under('launchd') } This gets us closer but not correct...
-  it { should be_enabled }
+USER=(`ac -p`.split(/\n/).map { |l| l.strip!.split(/\s{2,}/) }.sort_by { |a| a.last.to_f })[-2][0]
+
+describe service('com.buildkite.buildkite-agent-primary') do
+  # it { should be_running } # TODO: This is incorrectly using `ps aux` to look for service
+  # it { should be_running.under('launchd') } # This gets us closer but not quite
+  it { should be_enabled } # This works
 end
 
-# This handles checking the service is running as a workaround
+# Workaround to check for running launchd job
 describe command('launchctl list | grep com.buildkite.buildkite-agent-primary') do
   its(:exit_status) { should eq 0 }
 end
 
 describe file('/usr/local/bin/buildkite-agent') do
+  it { should be_executable }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'admin' }
   it { should be_mode 755 }
+end
+
+describe file("/Users/#{USER}/.buildkite-agent") do
+  it { should be_directory }
+  it { should be_owned_by "#{USER}" }
+  it { should be_grouped_into 'staff' }
+  it { should be_mode 755 }
+end
+
+describe file("/Users/#{USER}/.buildkite-agent/log") do
+  it { should be_directory }
+  it { should be_owned_by "#{USER}" }
+  it { should be_grouped_into 'staff' }
+  it { should be_mode 755 }
+end
+
+describe file("/Users/#{USER}/.buildkite-agent/builds") do
+  it { should be_directory }
+  it { should be_owned_by "#{USER}" }
+  it { should be_grouped_into 'staff' }
+  it { should be_mode 755 }
+end
+
+describe file("/Users/#{USER}/.buildkite-agent/hooks") do
+  it { should be_directory }
+  it { should be_owned_by "#{USER}" }
+  it { should be_grouped_into 'staff' }
+  it { should be_mode 755 }
+end
+
+describe file("/Users/#{USER}/.buildkite-agent/log/buildkite-agent.log") do
+  it { should be_file }
+  it { should be_owned_by "#{USER}" }
+  it { should be_grouped_into 'staff' }
+  it { should be_mode 644 }
+end
+
+describe file("/Users/#{USER}/.buildkite-agent/buildkite-agent.cfg") do
+  it { should be_file }
+  it { should be_owned_by "#{USER}" }
+  it { should be_grouped_into 'staff' }
+  it { should be_mode 644 }
 end
