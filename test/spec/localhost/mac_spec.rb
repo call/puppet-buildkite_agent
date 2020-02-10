3283d2ab6c69f07e1b2b46d3311149775f7edc15
call <6537446+call@users.noreply.github.com>
2020-02-09 22:52:14 -0800

    Add running test

--- test/spec/localhost/mac_spec.rb
+++ test/spec/localhost/mac_spec.rb
@@ -4,8 +4,5 @@ set :backend, :exec
 
 describe service('com.buildkite.buildkite-agent-primary'), :if => os[:family] == 'darwin', :sudo => true do
   it { should be_enabled }
+  it { should be_running }
 end
-
-# describe service('buildkite-agent start'), :if => os[:family] == 'darwin', :sudo => true do
-#   it { should be_running }
-# end
